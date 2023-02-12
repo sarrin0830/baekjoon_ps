3b63ff820f270d97b87d8850b709f9fdf5fb162f
sarrin0830 <121375953+sarrin0830@users.noreply.github.com>
2023-02-12 15:53:11 +0900

    [Bronze III] Title: 소수가 아닌 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27465. 소수가 아닌 수/소수가 아닌 수.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long long int n;
+    cin>>n;
+    if(n==1||n==2||n==3){
+        cout<<4;
+    }
+    else if(n%2==0){
+        cout<<n;
+    }
+    else{
+        cout<<n+1;    
+    }
+    
+
+    return 0;
+}
