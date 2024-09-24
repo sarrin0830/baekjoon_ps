a1f2be963d767513d5fd0160e8f6bc049f960f7d
sarrin <121375953+sarrin0830@users.noreply.github.com>
2024-09-24 19:11:14 +0900

    [Silver II] Title: 최대 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int x, n;
+	priority_queue<int> q;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> x;
+		if (x == 0) {
+			if (q.empty()) {
+				cout << 0 << '\n';
+				continue;
+			}
+			else {
+				cout << q.top() << '\n';
+				q.pop();
+			}
+		}
+		else {
+			q.push(x);
+		}
+	}
+
+
+	return 0;
+}
